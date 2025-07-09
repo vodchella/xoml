70da41e519a5ce64f1dd2f104528f01ad302b4f6
vodchella <twister.kz@gmail.com>
2025-07-09 23:46:50 +0500

    Some refactor

--- src/xo.ml
+++ src/xo.ml
@@ -1,3 +1,5 @@
+let printf = Printf.printf
+
 type input_result =
     | Move of string
     | Invalid of string
@@ -7,17 +9,18 @@ type game = {
     last_tip : string
 }
 
-let printf = Printf.printf
-
 let board_width   = 10
 let board_height  = 10
 let input_vmargin = board_height + 5
+let input_prompt  = "?> "
 let ascii_esc     = "\x1b"
 let inital_tip    = "Type your move (e.g.: 'E4') or ['q' | 'quit' | 'exit'] to exit"
 let inital_game   = {
     last_tip = inital_tip
 }
 
+
+(* Utility functions *)
 
 let screen_clear () =
     Sys.command "clear"
@@ -31,6 +34,23 @@ let print_at (str: string) row col =
     cursor_move row col;
     printf "%s" str
     |> ignore
+
+let move_is_valid_input_str str =
+    match String.length str with
+    | 2 ->
+        let c1 = String.get str 0 in
+        let c2 = String.get str 1 in
+        let is_letter c =
+          let code = Char.code c in
+          (code >= Char.code 'A' && code <= Char.code 'Z') ||
+          (code >= Char.code 'a' && code <= Char.code 'z')
+        in
+        let is_digit c =
+          let code = Char.code c in
+          code >= Char.code '0' && code <= Char.code '9'
+        in
+        is_letter c1 && is_digit c2
+    | _ -> false
 
 
 (* Board drawing functions *)
@@ -67,26 +87,9 @@ let board_draw () =
 
 (* Main functions *)
 
-let is_valid_move_str str =
-    match String.length str with
-    | 2 ->
-        let c1 = String.get str 0 in
-        let c2 = String.get str 1 in
-        let is_letter c =
-          let code = Char.code c in
-          (code >= Char.code 'A' && code <= Char.code 'Z') ||
-          (code >= Char.code 'a' && code <= Char.code 'z')
-        in
-        let is_digit c =
-          let code = Char.code c in
-          code >= Char.code '0' && code <= Char.code '9'
-        in
-        is_letter c1 && is_digit c2
-    | _ -> false
-
 let process_input str =
     match str with
-    | s when (is_valid_move_str s) -> Move str
+    | s when (move_is_valid_input_str s) -> Move str
     | "q" | "quit" | "exit" -> Quit
     | _   -> Invalid ("Invalid move: " ^ str)
 
@@ -94,17 +97,17 @@ let rec input_loop (g: game) =
     screen_clear ();
     board_draw ();
     print_at (g.last_tip) input_vmargin 1;
-    print_at "?> " (input_vmargin + 1) 1;
+    print_at input_prompt (input_vmargin + 1) 1;
     flush stdout;
     let input = read_line () in
     match process_input (String.trim input |> String.lowercase_ascii) with
+    | Invalid invalid_input ->
+        let new_game = { last_tip = invalid_input } in
+        input_loop new_game
+    | Move move ->
+        let new_game = { last_tip = "Your move: " ^ move } in
+        input_loop new_game
     | Quit -> ()
-    | Invalid inv ->
-        let new_game = { last_tip = inv } in
-        input_loop new_game
-    | Move m ->
-        let new_game = { last_tip = "Your move: " ^ m } in
-        input_loop new_game
 
 let () =
     input_loop inital_game
