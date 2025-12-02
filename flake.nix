3a6320caf7d2153d4d3c047a9a706fab20f66513
vodchella <twister.kz@gmail.com>
2025-12-02 19:16:14 +0000

    `build`: fix `flake.nix`

--- flake.nix
+++ flake.nix
@@ -3,18 +3,23 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: {
-    devShell = {
-      default = let
-        pkgs = import nixpkgs { system = builtins.currentSystem; };
-      in
-        pkgs.mkShell {
-          buildInputs = with pkgs; [
-            ocaml
-            dune
-            ocamlPackages.findlib
-          ];
-        };
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              ocaml
+              dune_3
+              ocamlPackages.findlib
+            ];
+          };
+        }
+      );
     };
-  };
 }
