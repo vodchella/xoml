d3e74a16187c1f41705f0d1ce778c470d8ec568f
vodchella <twister.kz@gmail.com>
2025-12-19 09:29:57 +0000

    Rename `xoml_test_score_board` to `xoml_test_board`

--- test/xoml_tests.ml
+++ test/xoml_tests.ml
@@ -2,6 +2,6 @@ let () =
     Alcotest.run "all_tests" [
         Xoml_test_directions.suite;
         Xoml_test_score_line.suite;
-        Xoml_test_score_board.suite;
+        Xoml_test_board.suite;
     ]
 
