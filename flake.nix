b50979e8f0a7254140fcd0017709377e3f577920
vodchella <twister.kz@gmail.com>
2026-02-02 14:36:46 +0000

    `release`: bump version to 0.1.7

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
           staticPkgs = pkgs.pkgsStatic;
           mkPkg = ocamlPkgs: ocamlPkgs.buildDunePackage {
             pname = "xoml";
-            version = "0.1.6";
+            version = "0.1.7";
             src = ./.;
           };
         in {
