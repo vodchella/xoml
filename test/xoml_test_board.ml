120e6c28be9ac78d701821ae8e8f5ad00887fd12
vodchella <twister.kz@gmail.com>
2026-06-21 22:13:17 +0000

    `test`: remove obsolete tests

--- test/xoml_test_board.ml
+++ test/xoml_test_board.ml
@@ -77,98 +77,6 @@ let test_score_board_with_type1_fork () =
     let score = score_board_easy g X in
     Alcotest.(check int) "score must be 2036" 2036 score
 
-let recognize_type2_fork_threat () =
-    let g = init_test_board () in
-    let g = apply_move g X "C6" in
-    let g = apply_move g X "E4" in
-    let g = apply_move g X "D3" in
-    let g = apply_move g X "G3" in
-    check_best_move g O "F3"
-
-let recognize_4_in_row_threat_1 () =
-    let g = init_test_board () in
-    let g = apply_move g X "D7" in
-    let g = apply_move g X "C6" in
-    let g = apply_move g O "D6" in
-    let g = apply_move g X "E6" in
-    let g = apply_move g X "F6" in
-    let g = apply_move g O "D5" in
-    let g = apply_move g X "E5" in
-    let g = apply_move g X "B4" in
-    let g = apply_move g O "C4" in
-    let g = apply_move g O "D4" in
-    let g = apply_move g O "E4" in
-    let g = apply_move g O "F4" in
-    let g = apply_move g X "G4" in
-    check_best_move g O "F5"
-
-let recognize_4_in_row_threat_2 () =
-    let g = init_test_board () in
-    let g = apply_move g X "G7" in
-    let g = apply_move g O "E6" in
-    let g = apply_move g X "E5" in
-    let g = apply_move g O "F5" in
-    let g = apply_move g X "H5" in
-    let g = apply_move g X "C4" in
-    let g = apply_move g X "D4" in
-    let g = apply_move g X "E4" in
-    let g = apply_move g O "F4" in
-    let g = apply_move g O "H4" in
-    let g = apply_move g X "I4" in
-    let g = apply_move g O "D3" in
-    let g = apply_move g X "E3" in
-    let g = apply_move g O "F3" in
-    let g = apply_move g X "G3" in
-    let g = apply_move g X "H3" in
-    let g = apply_move g O "C2" in
-    let g = apply_move g O "D2" in
-    let g = apply_move g O "E2" in
-    let g = apply_move g X "F2" in
-    let g = apply_move g O "D1" in
-    let g = apply_move g X "E1" in
-    let g = apply_move g O "F1" in
-    let g = apply_move g O "G1" in
-    let g = apply_move g X "C0" in
-    check_best_move g O "F6"
-
-let recognize_4_in_row_threat_3 () =
-    let g = init_test_board () in
-    let g = apply_move g X "E5" in
-    let g = apply_move g X "G3" in
-    let g = apply_move g X "H2" in
-    let g = apply_move g X "C7" in
-    let g = apply_move g O "C6" in
-    let g = apply_move g O "B5" in
-    let g = apply_move g O "C5" in
-    let g = apply_move g X "D5" in
-    let g = apply_move g X "F5" in
-    let g = apply_move g O "C4" in
-    let g = apply_move g O "D4" in
-    let g = apply_move g X "E4" in
-    let g = apply_move g X "B3" in
-    let g = apply_move g O "C3" in
-    let g = apply_move g O "D3" in
-    let g = apply_move g O "E3" in
-    let g = apply_move g O "F3" in
-    let g = apply_move g X "C2" in
-    let g = apply_move g O "D2" in
-    let g = apply_move g X "E2" in
-    let g = apply_move g X "F2" in
-    check_best_move g O "F4"
-
-let recognize_4_in_row_threat_with_fork () =
-    let g = init_test_board () in
-    let g = apply_move g X "E6" in
-    let g = apply_move g O "F5" in
-    let g = apply_move g X "D5" in
-    let g = apply_move g O "F7" in
-    let g = apply_move g X "F4" in
-    let g = apply_move g O "E7" in
-    let g = apply_move g X "D6" in
-    let g = apply_move g O "G7" in
-    let g = apply_move g X "D7" in
-    check_best_move g O "D4"
-
 let recognize_next_move_loss () =
     let g = init_test_board () in
     let g = apply_move g X "D7" in
@@ -226,11 +134,6 @@ let suite : string * unit Alcotest.test_case list =
         Alcotest.test_case "Score simple board"                  `Quick test_score_board;
         Alcotest.test_case "Score board with 41H+33 fork"        `Quick test_score_board_with_41H_33_fork;
         Alcotest.test_case "Score board with type1 fork"         `Quick test_score_board_with_type1_fork;
-        Alcotest.test_case "Recognize type2 fork threat"         `Quick recognize_type2_fork_threat;
-        Alcotest.test_case "Recognize 4-in-row threat (1)"       `Quick recognize_4_in_row_threat_1;
-        Alcotest.test_case "Recognize 4-in-row threat (2)"       `Quick recognize_4_in_row_threat_2;
-        Alcotest.test_case "Recognize 4-in-row threat (3)"       `Quick recognize_4_in_row_threat_3;
-        Alcotest.test_case "Recognize 4-in-row threat with fork" `Quick recognize_4_in_row_threat_with_fork;
         Alcotest.test_case "Recognize a loss on the next move"   `Quick recognize_next_move_loss;
         Alcotest.test_case "Check for a useless move"            `Quick check_for_not_useless_move;
     ]
